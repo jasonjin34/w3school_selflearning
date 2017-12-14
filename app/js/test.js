5c04505f1fe2658d509d525c23a25d7468210975
Jason Jin <hikigjack34@gmail.com>
2017-12-14 18:58:06 +0100

    add more bootstrap4 feature

--- app/js/test.js
+++ app/js/test.js
@@ -5,18 +5,45 @@ document.getElementById("demo").innerHTML = "Hello Nimo";
 function fSplittext() {
     var str = "a,b,c,d,e,f";
     var str_array = str.split(",");
-    var str_add;
-    for(var i = 0; i < 5; i++)
-    {
-        str_add =+ str_array[i];
+    var str_add = "";
+    for (var i = 0; i < 5; i++) {
+        str_add += str_array[i];
     }
     document.getElementById("split").innerHTML = str_add;
 }
 
+//filter 
+var ages = [32,33, 12,48];
+function checkAdult(age){
+    return age >= document.getElementById("ageTocheck").value;
+}
+
+function filterfunction(){
+    document.getElementById("filter").innerHTML = ages.filter(checkAdult);
+}
+
+var words = ['spray','limit', 'elite','exuberant', 'destruction','present'];
+
+function selecttext(words){
+    return words.length > 6;
+}
+
+function filter_mdn(){
+    document.getElementById("filter2").innerHTML = words.filter(selecttext);
+}
+
+//JavaScript Array reduce() method
+var numbers = [65, 44, 12, 4];
+
+function getSum(total, currentvalue) {
+    return total - currentvalue;
+}
+
+function fReduce() {
+    document.getElementById("reduce").innerHTML = numbers.reduce(getSum, -1);
+}
 
 
-
-//
 
 //String Length
 var txt = "ABCDE";
@@ -60,7 +87,16 @@ console.log(text1.concat(" ", text2));
 //Converting a string to an array
 var txt = "a,b,c,d,e";
 var array_txt = txt.split(",");
-for(var i = 0; i < 5; i++)
-{
+for (var i = 0; i < 5; i++) {
     console.log(array_txt[i]);
 }
+
+console.log(Math.floor(Math.random()*10)); //
+
+for(var i = 0; i < 10; i++)
+{
+    console.log(Math.random());
+}
+
+console.log(Math.floor(Math.random()*100)+1);
+
