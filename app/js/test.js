06339c5d5c0d425c7b3a0fd664acb7f1a1bd77b4
Jason Jin <hikigjack34@gmail.com>
2017-12-14 17:29:47 +0100

    bootstrap 4 learning

--- app/js/test.js
+++ app/js/test.js
@@ -1,3 +1,66 @@
- document.getElementById("demo").innerHTML = "Hello Nimo";
+document.getElementById("demo").innerHTML = "Hello Nimo";
 
- 
+
+//split test function
+function fSplittext() {
+    var str = "a,b,c,d,e,f";
+    var str_array = str.split(",");
+    var str_add;
+    for(var i = 0; i < 5; i++)
+    {
+        str_add =+ str_array[i];
+    }
+    document.getElementById("split").innerHTML = str_add;
+}
+
+
+
+
+//
+
+//String Length
+var txt = "ABCDE";
+console.log(txt.length);
+
+//Special Characters
+var teststring = "Vikings";
+var y = "We are the so-called" + teststring + "from the north";
+console.log(y);
+
+//JavaScript string methods
+var str = "Please the locate where 'locate' occurs!";
+console.log(str.indexOf("locate"));
+
+var str = "please locate where 'locate' occurs !";
+var pos = str.indexOf("locate", 15);
+console.log(str.search("locate"));
+
+//Extracting string parts
+
+var str = "apple, banana, kiwi";
+var res = str.slice(7);
+console.log(str);
+
+//replacing string content
+var n = str.replace("apple", "orange");
+console.log(n);
+
+str = "I hate apple and apple";
+console.log(str.replace(/apple/g, "orange"));
+
+var sTest = "i hate Apple and aPPLE";
+console.log(sTest.replace(/apple/gi, "orange"));
+console.log(sTest.toUpperCase());
+
+var text1 = "hello";
+var text2 = "world";
+console.log(text1.concat(" ", text2));
+
+
+//Converting a string to an array
+var txt = "a,b,c,d,e";
+var array_txt = txt.split(",");
+for(var i = 0; i < 5; i++)
+{
+    console.log(array_txt[i]);
+}
